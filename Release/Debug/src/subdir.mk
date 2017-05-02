0a13bb1ab6314644c323062892abb7c2cda052c4
Michele Sponchiado <mikesponk@gmail.com>
2017-05-02 15:12:13 +0200

    compiled the packages for the version 3.0.0 of the CC2650 firmware

---
+++ Release/Debug/src/subdir.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../Debug/src/sblAppEx.o \
+../Debug/src/sbl_device.o \
+../Debug/src/sbl_device_cc2650.o \
+../Debug/src/sbllib.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
